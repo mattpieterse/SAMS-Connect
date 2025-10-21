f4dd33e607793f12d7f08036c3879edb4be1a1e5
mattpieterse <63480247+mattpieterse@users.noreply.github.com>
2025-10-22 00:24:13 +0200

    feat(app): configure and register the static serilog logger

--- Connect.UI/App.xaml.cs
+++ Connect.UI/App.xaml.cs
@@ -3,6 +3,7 @@ using CommunityToolkit.Mvvm.DependencyInjection;
 using Connect.UI.Shells;
 using JetBrains.Annotations;
 using Microsoft.Extensions.DependencyInjection;
+using Serilog;
 using Wpf.Ui.Appearance;
 using ThemeService = Connect.UI.Services.ThemeService;
 
@@ -17,6 +18,11 @@ public sealed partial class App
     /// </summary>
     [UsedImplicitly]
     private void OnStartup(object sender, StartupEventArgs e) {
+        Log.Logger = GetLogsConfiguration().CreateLogger();
+        Log.Information(
+            "Application started successfully."
+        );
+
         Ioc.Default.ConfigureServices(
             serviceProvider: GetServiceCollection().BuildServiceProvider()
         );
@@ -24,6 +30,12 @@ public sealed partial class App
         UseApplicationThemes();
         var window = Ioc.Default.GetRequiredService<Shell>();
         window.Show();
+    }
+
+
+    protected override void OnExit(ExitEventArgs e) {
+        Log.CloseAndFlush();
+        base.OnExit(e);
     }
 
 #endregion
@@ -39,6 +51,18 @@ public sealed partial class App
             .AddSingleton<ThemeService>();
 
         return services;
+    }
+
+
+    private static LoggerConfiguration GetLogsConfiguration() {
+        var configuration = new LoggerConfiguration();
+
+        configuration
+            .MinimumLevel.Debug()
+            .WriteTo.File("logs.txt", rollingInterval: RollingInterval.Day)
+            .WriteTo.Console();
+
+        return configuration;
     }
 
 
--- Connect.UI/Shells/Shell.xaml.cs
+++ Connect.UI/Shells/Shell.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using ReactiveUI;
+using Serilog;
 
 namespace Connect.UI.Shells;
 
