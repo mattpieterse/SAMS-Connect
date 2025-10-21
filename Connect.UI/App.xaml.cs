0849ac8c647e067c4959fc03c97157de30d18b30
mattpieterse <63480247+mattpieterse@users.noreply.github.com>
2025-10-22 00:00:31 +0200

    feat(app): create and register the custom theme service as a singleton

--- Connect.UI/App.xaml.cs
+++ Connect.UI/App.xaml.cs
@@ -1,10 +1,53 @@
-﻿using System.Configuration;
-using System.Data;
-using System.Windows;
+﻿using System.Windows;
+using CommunityToolkit.Mvvm.DependencyInjection;
+using JetBrains.Annotations;
+using Microsoft.Extensions.DependencyInjection;
+using Wpf.Ui.Appearance;
+using ThemeService = Connect.UI.Services.ThemeService;
 
 namespace Connect.UI;
 
-/// <summary>
-/// Interaction logic for App.xaml
-/// </summary>
-public partial class App : Application { }
+public sealed partial class App
+{
+#region Lifecycle
+
+    /// <summary>
+    /// The main entry-point for the application.
+    /// </summary>
+    [UsedImplicitly]
+    private void OnStartup(object sender, StartupEventArgs e) {
+        Ioc.Default.ConfigureServices(
+            serviceProvider: GetServiceCollection().BuildServiceProvider()
+        );
+
+        UseApplicationThemes();
+        var window = Ioc.Default.GetRequiredService<Shell>();
+            window.Show();
+    }
+
+#endregion
+
+#region Internals
+
+    private static ServiceCollection GetServiceCollection() {
+        var services = new ServiceCollection();
+
+        services
+            .AddScoped<Shell>()
+            .AddSingleton<ThemeService>();
+
+        return services;
+    }
+
+
+    private static void UseApplicationThemes() {
+        var globalThemeService = Ioc.Default.GetService<ThemeService>();
+        globalThemeService?.Listen();
+
+        ApplicationThemeManager.Apply(
+            applicationTheme: ApplicationThemeManager.GetAppTheme()
+        );
+    }
+
+#endregion
+}
